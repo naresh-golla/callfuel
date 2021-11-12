f0ea7efd43e12c422460edc828380b96ac7bde3d
Naresh Golla <spicynaresh.143@gmail.com>
2021-11-12 13:01:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  useEffect(() => {
+    window.location.replace("https://spetrol.in/");
+  });
+  return <div className="App"></div>;
+}
